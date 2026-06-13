6643de60a108fb979cec1cb733fefed737ed726f
kamilstanik516-cmd <kamilstanik516@gmail.com>
2026-06-13 05:55:46 +0000

    nowy

--- components/retro-go/targets/esp32-s3-devkit/config.h
+++ components/retro-go/targets/esp32-s3-devkit/config.h
@@ -11,6 +11,9 @@
 #define RG_GPIO_LCD_MISO 12
 #define RG_GPIO_LCD_MOSI 13
 #define RG_GPIO_LCD_CLK 14
+#define RG_GPIO_LCD_CS 15
+#define RG_SCREEN_HOST SPI2_HOST
+#define RG_SCREEN_SPEED SPI_MASTER_FREQ_40M
 
 // Piny ekranu (Twoje sprawdzone)
 #define RG_PIN_TFT_SCLK     14
