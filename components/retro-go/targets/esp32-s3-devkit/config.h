e8e7374ad04404bb95860277bb51c5359c8c865b
kamilstanik516-cmd <kamilstanik516@gmail.com>
2026-06-14 07:04:54 +0000

    jjj

--- components/retro-go/targets/esp32-s3-devkit/config.h
+++ components/retro-go/targets/esp32-s3-devkit/config.h
@@ -4,16 +4,25 @@
 #define RG_TARGET_TITLE "Moj ESP32-S3 Game Boy"
 
 // Ekran ST7789
-#define RG_DISPLAY_DRIVER st7789
+#define RG_SCREEN_DRIVER 0
 #define RG_SCREEN_WIDTH 320
 #define RG_SCREEN_HEIGHT 240
+#define RG_SCREEN_HOST SPI2_HOST     
 #define RG_GPIO_LCD_DC 2
 #define RG_GPIO_LCD_MISO -1
 #define RG_GPIO_LCD_MOSI 13
 #define RG_GPIO_LCD_CLK 14
 #define RG_GPIO_LCD_CS 15
-#define RG_SCREEN_HOST SPI2_HOST
 #define RG_SCREEN_SPEED SPI_MASTER_FREQ_20M
+
+// Ręczna sekwencja startowa (Inwersja i Rotacja)
+#define RG_SCREEN_INIT() \
+    ILI9341_CMD(0x11); \
+    ILI9341_CMD(0x36, 0x60); \
+    ILI9341_CMD(0x3A, 0x55); \
+    ILI9341_CMD(0x21); \
+    ILI9341_CMD(0x29);
+
 
 
 
